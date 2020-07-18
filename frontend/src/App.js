c71d06d20092e89f0f4f6cde2fa3360d1ce2a5f7
Julia Chen <juliazychen@gmail.com>
2020-07-18 19:12:38 -0400

    Added React

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>SnackTrack</h1>
+      <img className="logo" src="/images/logo.png" alt="Our logo">
+        <div className="landing">
+          <a href="login.html"><button>Login</button></a>
+          <a href="userTypeSignup.html"><button>Sign Up</button></a>
+        </div>
+	  </div>
+  );
+}
+
+export default App;
