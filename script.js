10d253fd30473a9d2367b339cede721c31b54de7
Julia Chen <juliazychen@gmail.com>
2020-07-18 16:23:46 -0400

    Initial Files

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  return
+});
+
+function setUserType(type){
+  if (type==1){
+    console.log("I'm a receiver!")
+  }
+  else{
+    console.log("I'm a restaurant!")
+  }
+}
