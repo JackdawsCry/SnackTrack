860472d79a152107e223b0ca49cf61f8eefe21f9
Donovan Yohan <donovanyohan@gmail.com>
2020-07-18 19:42:28 -0400

    fix image structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <h1>SnackTrack</h1>
-      <img className="logo" src="/images/logo.png" alt="Our logo">
-        <div className="landing">
-          <a href="login.html"><button>Login</button></a>
-          <a href="userTypeSignup.html"><button>Sign Up</button></a>
-        </div>
-	  </div>
+      <img className="logo" src="/logo.png" alt="Our logo" />
+      <div className="landing">
+        <a href="login.html">
+          <button>Login</button>
+        </a>
+        <a href="userTypeSignup.html">
+          <button>Sign Up</button>
+        </a>
+      </div>
+    </div>
   );
 }
 
