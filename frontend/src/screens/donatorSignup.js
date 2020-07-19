e9192ff79e26f67386626e00f4e8439d808dc2f7
Julia Chen <juliazychen@gmail.com>
2020-07-18 23:59:27 -0400

    Added css

--- frontend/src/screens/donatorSignup.js
+++ frontend/src/screens/donatorSignup.js
@@ -8,22 +8,29 @@ function donatorSignup(props) {
       <img class="logo" src="img/logo.png" alt="Our logo" />
       <h2>Restaurant Sign Up</h2>
       <div class="donatorSignup">
-        <label>Restaurant Name: </label>
-        <input/>
-        <p></p>
-        <label>Phone Number: </label>
-        <input/>
-        <p></p>
-        <label>Address: </label>
-        <input/>
-        <p></p>
-        <label>Username: </label>
-        <input/>
-        <p></p>
-        <label>Password: </label>
-        <input/>
-        <p></p>
-        <button>Submit</button>
+        <div class="inputField">
+          <label>Restaurant Name: </label>
+          <input />
+        </div>
+        <div class="inputField">
+          <label>Phone Number: </label>
+          <input />
+        </div>
+        <div class="inputField">
+          <label>Address: </label>
+          <input />
+        </div>
+        <div class="inputField">
+          <label>Username: </label>
+          <input />
+        </div>
+        <div class="inputField">
+          <label>Password: </label>
+          <input />
+        </div>
+        <div class="inputField">
+          <button>Submit</button>
+        </div>
       </div>
     </div>
   );
--- frontend/src/screens/signup.js
+++ frontend/src/screens/signup.js
@@ -7,19 +7,25 @@ function signup(props) {
       <img class="logo" src="img/logo.png" alt="Our logo" />
       <h2>User Sign Up</h2>
       <div class="signup">
-        <label>Full Name: </label>
-        <input/>
-        <p></p>
-        <label>Phone Number: </label>
-        <input/>
-        <p></p>
-        <label>Username: </label>
-        <input/>
-        <p></p>
-        <label>Password: </label>
-        <input type="text" id="fname" name="fname" />
-        <p></p>
-        <button>Submit</button>
+        <div class="inputField">
+          <label>Full Name: </label>
+          <input />
+        </div>
+        <div class="inputField">
+          <label>Phone Number: </label>
+          <input />
+        </div>
+        <div class="inputField">
+          <label>Username: </label>
+          <input />
+        </div>
+        <div class="inputField">
+          <label>Password: </label>
+          <input type="text" id="fname" name="fname" />
+        </div>
+        <div class="inputField">
+          <button>Submit</button>
+        </div>
       </div>
     </div>
   );
