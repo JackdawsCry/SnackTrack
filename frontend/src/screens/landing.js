b06345a1510a8895895b370be6fdaf7c12889430
Julia Chen <juliazychen@gmail.com>
2020-07-18 22:19:47 -0400

    Deleted space

--- frontend/src/screens/landing.js
+++ frontend/src/screens/landing.js
@@ -10,7 +10,6 @@ function landing(props) {
         <Link to="/login">
           <button>Login</button>
         </Link>
-
         <Link to="/userTypeSignup">
           <button>Sign Up</button>
         </Link>
