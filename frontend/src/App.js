9ee33683d9e5f2d121945aa6fc82daa43ee9d8e7
Julia Chen <juliazychen@gmail.com>
2020-07-18 21:56:21 -0400

    Revert "add state for login"
    This reverts commit d40b07ce1dbf55f996422033ad9915eea0ea4896.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import landing from "./screens/landing";
+
 import browse from "./screens/browse";
 import donate from "./screens/donate";
-import userTypeSignup from "./screens/userTypeSignup";
 import login from "./screens/login";
+import userTypeLogin from "./screens/userTypeLogin";
+import userTypeSignup from"./screens/userTypeSignup";
 
 function App() {
-  const [userType, setUserType] = useState("user");
-  const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <BrowserRouter>
-      <Route
-        exact
-        path="/"
-        component={loggedIn ? (userType == "user" ? browse : donate) : landing}
-      />
-      <Route path="/userTypeSignup" component={userTypeSignup} />
+    <BrowserRouter>    
       <Route path="/login" component={login} />
+      <Route exact path="/" component={landing} />
+      <Route path="/userTypeLogin" component={userTypeLogin} />
+      <Route path="/userTypeSignup" component={userTypeSignup}/>
     </BrowserRouter>
   );
 }
