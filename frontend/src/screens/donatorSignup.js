37ac247f14e62be61dfd006238b5a5f555b842c7
UpTheAnte <caroline.ma52@gmail.com>
2020-07-18 23:25:52 -0400

    Update userTypeSignup.js

---
+++ frontend/src/screens/donatorSignup.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+function donatorSignup(props) {
+  return (
+    <div className="container">
+      <h1>SnackTrack</h1>
+        <img class="logo" src="img/logo.png" alt="Our logo"/>
+        <h2>Restaurant Sign Up</h2>
+        <div class="donatorSignup">
+            <label for="fname">Restaurant Name:</label>
+            <input type="text" id="fname" name="fname"/>
+            <p></p>
+            <label for="fname">Phone Number:</label>
+            <input type="text" id="fname" name="fname"/>
+            <p></p>
+            <label for="fname">Address:</label>
+            <input type="text" id="fname" name="fname"/>
+            <p></p>
+            <label for="fname">Username:</label>
+            <input type="text" id="fname" name="fname"/>
+            <p></p>
+            <label for="fname">Password:</label>
+            <input type="text" id="fname" name="fname"/>
+            <p>
+            </p>
+            <button>Submit</button>
+        </div>
+    </div>
+  );
+}
+
+export default donatorSignup;
