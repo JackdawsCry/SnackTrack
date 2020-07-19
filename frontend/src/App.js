640f12dc230d201bee65fb3e3f90603579c0456e
Julia Chen <juliazychen@gmail.com>
2020-07-18 20:09:12 -0400

    Added login button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import login from './screens/login';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
       <h1>SnackTrack</h1>
       <img className="logo" src="img/logo.png" alt="Our logo" />
       <div className="landing">
+      <Route path="/login/:id" component={login} />
         <a href="login.html">
-          <button>Login</button>
+          
+          <button><Link to="/">Login</Link></button>
         </a>
         <a href="userTypeSignup.html">
           <button>Sign Up</button>
         </a>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
