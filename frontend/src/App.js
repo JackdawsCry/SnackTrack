d40b07ce1dbf55f996422033ad9915eea0ea4896
Donovan Yohan <donovanyohan@gmail.com>
2020-07-18 21:27:53 -0400

    add state for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import landing from "./screens/landing";
+import browse from "./screens/browse";
+import donate from "./screens/donate";
 import userTypeLogin from "./screens/userTypeLogin";
-import userTypeSignup from"./screens/userTypeSignup";
+import userTypeSignup from "./screens/userTypeSignup";
 
 function App() {
+  const [userType, setUserType] = useState("user");
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <BrowserRouter>
-      <Route exact path="/" component={landing} />
+      <Route
+        exact
+        path="/"
+        component={loggedIn ? (userType == "user" ? browse : donate) : landing}
+      />
       <Route path="/userTypeLogin" component={userTypeLogin} />
-      <Route path="/userTypeSignup" component={userTypeSignup}/>
+      <Route path="/userTypeSignup" component={userTypeSignup} />
     </BrowserRouter>
   );
 }
---
+++ frontend/src/screens/browse.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+function login(props) {
+  return (
+    <div className="container">
+      <h1> SnackTrack </h1>
+      <img className="logo" src="img/logo.png" alt="Our logo" />
+      <div>Browse</div>
+    </div>
+  );
+}
+
+export default login;
